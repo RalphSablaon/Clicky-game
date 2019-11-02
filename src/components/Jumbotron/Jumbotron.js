53f845d33f246297adb51ffa441d4552bb94fda3
Ralph Sablaon <rsablaon88@gmail.com>
2019-11-02 09:11:12 -0700

    minor changes to jumbo/scorecard

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -4,7 +4,7 @@ import "./Jumbotron.css";
 const Jumbotron = () => (
     <div className="jumbotron jumbotron-fluid">
       <div className="container text-center">
-        <h1 className="display-4">React Clicky Game!</h1>
+        <h1 className="display-4">React Clicky Game! Street Fighter Edition</h1>
         <p className="lead">Click on one of the fighters to earn points, but don't click on the same fighter more than once!</p>
       </div>
     </div>
