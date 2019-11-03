f3393cf58869d98bd571f7c17351fb3ca8d8d4fa
Ralph Sablaon <rsablaon88@gmail.com>
2019-11-02 18:56:22 -0700

    Updates

---
+++ precache-manifest.27dc98ca8cfdb643b86562c8d9f756db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ca1b0def955146e9bdce7aeda37f73",
+    "url": "/Clicky-game/index.html"
+  },
+  {
+    "revision": "26b07d02468a3e704b4a",
+    "url": "/Clicky-game/static/css/main.e5a9c0ef.chunk.css"
+  },
+  {
+    "revision": "858337689f8457637e8e",
+    "url": "/Clicky-game/static/js/2.05bcf8b3.chunk.js"
+  },
+  {
+    "revision": "26b07d02468a3e704b4a",
+    "url": "/Clicky-game/static/js/main.1f2f6e27.chunk.js"
+  },
+  {
+    "revision": "30c62ed0b7b5ea139304",
+    "url": "/Clicky-game/static/js/runtime-main.615d9a9e.js"
+  }
+]);
